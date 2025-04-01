d91106d86ee555773f5c2c93f5a5caaa6f9652b4
Mark Baran <mebaran@gmail.com>
2025-03-31 23:34:16 -0700

    Updated lsp keys to use keymapsOnEvents (LspAttach)

--- config/plugins/lsp.nix
+++ config/plugins/lsp.nix
@@ -9,44 +9,126 @@
       lightbulb.virtualText = false;
     };
   };
-  autoCmd = let
-    lspsaga = action: ''"<cmd>Lspsaga ${action}<cr>"'';
-  in [
+  autoCmd = [
     {
       desc = "Configure LSP keymaps and omnifunc";
       event = "LspAttach";
       callback.__raw = ''
         function(ev)
-            local function lsp_callback(bufnr)
-                -- Enable completion triggered by <c-x><c-o>
-                vim.bo[bufnr].omnifunc = "v:lua.vim.lsp.omnifunc"
-
-                -- Buffer local mappings.
-                local function opts_desc(desc)
-                    return { buffer = bufnr, desc = desc }
-                end
-                local map = vim.keymap.set
-                map("n", "gF", ${lspsaga "finder"}, opts_desc("Goto symbol finder"))
-                map("n", "gD", vim.lsp.buf.declaration, opts_desc("Goto declaration"))
-                map("n", "gd", vim.lsp.buf.definition, opts_desc("Goto definition"))
-                map("n", "gI", vim.lsp.buf.implementation, opts_desc("Goto implementation"))
-                map("n", "gr", vim.lsp.buf.references, opts_desc("Goto references"))
-                map("n", "K", ${lspsaga "hover_doc"}, {})
-                map({ "i", "n" }, "<C-k>", vim.lsp.buf.signature_help, opts_desc("Signature help"))
-                map("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts_desc("Add workspace folder"))
-                map("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts_desc("Remove workspace folder"))
-                map("n", "<leader>wl", function()
-                    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-                end, opts_desc("Print workspace folders list"))
-                map("n", "<leader>gd", ${lspsaga "peek_definition"}, opts_desc("Peek definition"))
-                map("n", "<leader>gy", ${lspsaga "peek_type_definition"}, opts_desc("Peek t(y)pe definition"))
-                map("n", "<leader>cr", vim.lsp.buf.rename, opts_desc("Code rename"))
-                map("n", "<leader>ca", ${lspsaga "code_action"}, opts_desc("Code action"))
-                map({ "n", "v" }, "<leader>=", vim.lsp.buf.format, opts_desc("Code format"))
-            end
-            lsp_callback(ev.buf)
+          -- Enable completion triggered by <c-x><c-o>
+          vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
         end
       '';
+    }
+  ];
+  keymapsOnEvents.LspAttach = let
+    lspsaga = action: "<cmd>Lspsaga ${action}<cr>";
+  in [
+    {
+      mode = "n";
+      key = "gF";
+      action = lspsaga "finder";
+      options.desc = "Goto symbol finder";
+      options.buffer = true;
+    }
+    {
+      mode = "n";
+      key = "gD";
+      action.__raw = "vim.lsp.buf.declaration";
+      options.desc = "Goto declaration";
+      options.buffer = true;
+    }
+    {
+      mode = "n";
+      key = "gd";
+      action.__raw = "vim.lsp.buf.definition";
+      options.desc = "Goto definition";
+      options.buffer = true;
+    }
+    {
+      mode = "n";
+      key = "gI";
+      action.__raw = "vim.lsp.buf.implementation";
+      options.desc = "Goto implementation";
+      options.buffer = true;
+    }
+    {
+      mode = "n";
+      key = "gr";
+      action.__raw = "vim.lsp.buf.references";
+      options.desc = "Goto references";
+      options.buffer = true;
+    }
+    {
+      mode = "n";
+      key = "K";
+      action = lspsaga "hover_doc";
+      options.buffer = true;
+    }
+    {
+      mode = ["i" "n"];
+      key = "<C-k>";
+      action.__raw = "vim.lsp.buf.signature_help";
+      options.desc = "Signature help";
+      options.buffer = true;
+    }
+    {
+      mode = "n";
+      key = "<leader>wa";
+      action.__raw = "vim.lsp.buf.add_workspace_folder";
+      options.desc = "Add workspace folder";
+      options.buffer = true;
+    }
+    {
+      mode = "n";
+      key = "<leader>wr";
+      action.__raw = "vim.lsp.buf.remove_workspace_folder";
+      options.desc = "Remove workspace folder";
+      options.buffer = true;
+    }
+    {
+      mode = "n";
+      key = "<leader>wl";
+      action.__raw = ''        function()
+                print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+            end'';
+      options.desc = "Print workspace folders list";
+      options.buffer = true;
+    }
+    {
+      mode = "n";
+      key = "<leader>gd";
+      action = lspsaga "peek_definition";
+      options.desc = "Peek definition";
+      options.buffer = true;
+    }
+    {
+      mode = "n";
+      key = "<leader>gy";
+      action = lspsaga "peek_type_definition";
+      options.desc = "Peek t(y)pe definition";
+      options.buffer = true;
+    }
+    {
+      mode = "n";
+      key = "<leader>cr";
+      action.__raw = "vim.lsp.buf.rename";
+      options.desc = "Code rename";
+      options.buffer = true;
+    }
+    {
+      mode = "n";
+      key = "<leader>ca";
+      action = lspsaga "code_action";
+      options.desc = "Code action";
+      options.buffer = true;
+    }
+    {
+      mode = ["n" "v"];
+      key = "<leader>=";
+      action.__raw = "vim.lsp.buf.format";
+      options.desc = "Code format";
+      options.buffer = true;
     }
   ];
   keymaps = [
--- lang/golang.nix
+++ lang/golang.nix
@@ -1,5 +1,4 @@
-{ pkgs, ... }:
-{
+{pkgs, ...}: {
   plugins.lsp.enable = true;
   plugins.lsp.servers.gopls.enable = true;
   extraPackages = with pkgs; [
