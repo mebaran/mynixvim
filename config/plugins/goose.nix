1d2af21438275a067d3dd59833f5de84b39c17c5
mebaran <mebaran@gmail.com>
2025-06-11 05:39:08 +0000

    Goose.nvim and flake bump

--- config/plugins/goose.nix
+++ config/plugins/goose.nix
@@ -6,7 +6,7 @@
         owner = "azorng";
         repo = "goose.nvim";
         rev = "main";
-        sha256 = "04inwi0iz9l862qpz5k9skc6pmdbniixm6g6zlcwb8zxy0i6q8ln";
+        sha256 = "1ry5kcx5kyq6kznknggxjwnbzjnsl09asnbxlcvk46qwhvd4s1qr";
       };
       doCheck = false;
     })
