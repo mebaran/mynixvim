d1866ec99daa7eba3c25eff8d87204e2fe5042f7
Mark Baran <mebaran@gmail.com>
2025-06-22 20:35:29 -0700

    Bumped goose

--- config/plugins/goose.nix
+++ config/plugins/goose.nix
@@ -6,7 +6,7 @@
         owner = "azorng";
         repo = "goose.nvim";
         rev = "main";
-        sha256 = "0h1q9nd2kkxvgbm163k5kf52r96r64vqccvdy45jjwa650vblbpk";
+        sha256 = "sha256-u7NabwnlwNixc0Axu50Kr81TkHMNbWKvCTTsPQascbA=";
       };
       doCheck = false;
     })
